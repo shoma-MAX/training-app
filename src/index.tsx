445f5d9cb7aaf616fb275951957f9058f64d71c7
shoma-MAX <shomakatsura@gmail.com>
2021-03-28 19:30:20 +0900

    firestoreからデータを取得して表示する。

--- src/index.tsx
+++ src/index.tsx
@@ -1,83 +1,64 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import React, { useState } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
 import './index.css';
 
-interface Component {
-    value: string;
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyDx9gHV4SLT3VJIZ9QcTXlg1so75QiInbo",
+    authDomain: "testdb-34890.firebaseapp.com",
+    databaseURL: "https://testdb-34890.firebaseio.com",
+    projectId: "testdb-34890",
+    storageBucket: "testdb-34890.appspot.com",
+    messagingSenderId: "747671733994",
+    appId: "1:747671733994:web:ac289ee551e44df09c7993",
+    measurementId: "G-Y7Q6F9VKFF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-class Square extends React.Component<{}, { value: string }>  {
+  function App(){
+    const [users,setUsers]:any[] =useState([]);
+    const handleClickFetchButton = async ()=>{
+      // document 取得
+      const db=firebase.firestore();
+      /*const doc = await db.collection('users').doc('CH1myjCXfnAV7xw8K7DG').get();
+      console.log(doc.data());
+      console.log('Fetch Clicked');
+      */
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            value: 'O'
-        };
+      // collection取得
+      const snapshot = await db
+      .collection('users')
+      .get()
+      const _users :any= [];
+      snapshot.forEach(doc => {
+        _users.push({
+          userId:doc.id,
+          ...doc.data()
+          
+        });
+      });
+      setUsers(_users);
     }
+    const userListItems =users.map((user: { userId: React.Key | null | undefined;name:string; age: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined; })=>{
+      return (
+        <li key={user.userId}> {user.name}:{user.age}:</li>
+      )
+    })
+  
+    return (
+      <div className = "App">
+        <h1 >Hello World</h1>
+        <button onClick={handleClickFetchButton}>取得</button>
+        <ul>{userListItems}</ul>
+      </div>
 
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: 'X' })}
-            >
-                {this.state.value}
-            </button>
-        );
-    }
-}
+    )
 
-class Board extends React.Component {
-    renderSquare(i: number) {
-        return <Square />;
-    }
+  }
 
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
+  
+render(<App />, document.getElementById("root"));
